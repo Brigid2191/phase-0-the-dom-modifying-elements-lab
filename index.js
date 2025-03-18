6b35ce88fff6b9a28654935c77da7770e3aa46fb
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-03-18 06:37:00 +0300

    Updated assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main> element with id "main"
+document.querySelector("main#main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id to "victory"
+newHeader.id = "victory";
+
+// Set its content to "YOUR-NAME is the champion"
+newHeader.textContent = "BRIGID is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
+
